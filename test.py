b15bbe8b131f9a22bf1b717090b4285580acacc4
tolik0 <iatsuk@ucu.edu.ua>
2018-02-24 15:55:28 +0200

    Minor changes

--- test.py
+++ test.py
@@ -7,8 +7,12 @@ import urllib.request
 import urllib.parse
 import json
 from hidden import oauth
+
 BASE_URL = "https://api.edamam.com/search?"
-params = {"q": "chicken"}
+search = input("Input what you want to look for: ")
+params = {"q": search}
+
+
 def get_data_from_URL(base_url, params):
     params.update(oauth())
     params_str = urllib.parse.urlencode(params)
@@ -18,8 +22,13 @@ def get_data_from_URL(base_url, params):
     js = json.loads(data)
     new_data = {}
     for i in js["hits"]:
-        new_data[i["recipe"]["label"]] = i["recipe"]["ingredientLines"]
-    with open("file.json", "w", encoding = "utf-8") as file:
-        json.dump(new_data, file, ensure_ascii = False, indent = 4)
+        label = i["recipe"]["label"]
+        new_data[label] = {}
+        new_data[label]["url"] = i["recipe"]["url"]
+        new_data[label]["ingredients"] = i["recipe"]["ingredients"]
+    with open("file.json", "w", encoding="utf-8") as file:
+        json.dump(new_data, file, ensure_ascii=False, indent=4)
     return new_data
+
+
 data = get_data_from_URL(BASE_URL, params)
