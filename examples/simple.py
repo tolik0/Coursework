7cad218e532e93d9b97ddb1a9ddd4e9e671b0dbd
Anatolii Iatsuk <35109939+tolik0@users.noreply.github.com>
2018-03-26 17:26:03 +0300

    Add files via upload

---
+++ examples/simple.py
@@ -0,0 +1,15 @@
+# import the necessary packages
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn import datasets
+from skimage import exposure
+import numpy as np
+
+mnist = datasets.load_digits()
+
+X_train, X_test, y_train, y_test = train_test_split(mnist.data, mnist.target, test_size=0.1, random_state=42)
+model = KNeighborsClassifier(n_neighbors=5)
+model.fit(X_train, y_train)
+score = model.score(X_test, y_test)
+print(score)
