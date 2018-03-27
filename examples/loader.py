3ef019a29840a6b261ecd29b4e776a5935422650
tolik0 <iatsuk@ucu.edu.ua>
2018-03-27 19:59:59 +0300

    Divide training and validation data

--- examples/loader.py
+++ examples/loader.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 
 
-def load_data(train_path, valid_path):
+def load_data(path):
     """
     TODO: Add documentation
     :param path:
@@ -12,20 +12,19 @@ def load_data(train_path, valid_path):
     labels = []
     X = []
     y = []
-    products = os.listdir(train_path)
+    products = os.listdir(path)
     for i, product in enumerate(products):
         print("Product {} [{}/{}] loaded...".format(product, i + 1,
                                                     len(products)))
         labels.append(product)
-        for path in (train_path, valid_path):
-            images = os.listdir(os.path.join(path, product))
-            for filename in images:
-                image = np.array(Image.open(os.path.join(path, product,
-                                                         filename)))
-                image.resize((25, 25))
-                image = image.flatten()
-                X.append(image)
-                y.append(i)
+        images = os.listdir(os.path.join(path, product))
+        for filename in images:
+            image = np.array(Image.open(os.path.join(path, product,
+                                                     filename)))
+            image.resize((50, 50))
+            image = image.flatten()
+            X.append(image)
+            y.append(i)
 
     X = np.array(X)
     y = np.array(y)
--- examples/simple.py
+++ examples/simple.py
@@ -4,9 +4,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn import datasets
 from loader import load_data
 
-X, y, labels = load_data("../fruits-360/Training", "../fruits-360/Validation")
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3333,
-                                                    random_state=42)
+X_train, y_train, labels = load_data("../fruits-360/Training")
+X_test, y_test, labels = load_data("../fruits-360/Validation")
 for k in range(1, 50):
     model = KNeighborsClassifier(n_neighbors=k)
     model.fit(X_train, y_train)
