995847b0ca8cb3235a21397eaa9407d12b74f9ad
tolik0 <iatsuk@ucu.edu.ua>
2018-06-14 22:46:04 +0300

    Minor changes

--- modules/prefinal.py
+++ modules/prefinal.py
@@ -73,20 +73,25 @@ def detector(model, image_path, to_image_url):
 
     def find_objects(path):
         ingr = []
-        boundaries = [(np.array([10, 20, 5]), np.array([28, 255, 255])),
+        boundaries = [(np.array([0, 0, 0]), np.array([179, 255, 20])),
+                      # black
+                      (np.array([10, 10, 10]), np.array([18, 255, 255])),
+                      # orange
+                      (np.array([18, 10, 10]), np.array([28, 255, 255])),
                       # yellow
-                      (np.array([28, 20, 5]), np.array([85, 255, 255])),
+                      (np.array([28, 10, 10]), np.array([85, 255, 255])),
                       # green
-                      (np.array([85, 20, 5]), np.array([140, 255, 255])),
+                      (np.array([85, 10, 10]), np.array([140, 255, 255])),
                       # blue
-                      (np.array([140, 20, 5]), np.array([160, 255, 255])),
+                      (np.array([140, 10, 10]), np.array([160, 255, 255])),
                       # pink
-                      ((np.array([160, 20, 5]), np.array([179, 255, 255])),
+                      ((np.array([160, 10, 10]), np.array([179, 255, 255])),
                        # red
-                       (np.array([0, 20, 5]), np.array([10, 255, 255])))]
+                       (np.array([0, 10, 10]), np.array([13, 255, 255])))]
 
         image = cv2.imread(path)
-        bordersize = 50
+        image_size = image.shape[0] * image.shape[1]
+        bordersize = int(0.2 * np.sqrt(image_size))
         image = cv2.copyMakeBorder(image, top=bordersize, bottom=bordersize,
                                    left=bordersize, right=bordersize,
                                    borderType=cv2.BORDER_CONSTANT,
@@ -104,22 +109,23 @@ def detector(model, image_path, to_image_url):
                 print(0)
             else:
                 mask = cv2.inRange(hsv, lower, upper)
-
             im2, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE,
                                                         cv2.CHAIN_APPROX_SIMPLE)
-            new_contours = []
-
-            for i in range(len(contours)):
-                if len(contours[i]) > 50:
-                    new_contours.append(contours[i])
-
-            contours = new_contours
 
             for contour in contours:
                 rect = cv2.minAreaRect(contour)
 
                 box = cv2.boxPoints(rect)
                 box = np.int0(box)
+
+                size = np.sqrt((box[0][0] - box[1][0]) ** 2 +
+                               (box[0][1] - box[1][1]) ** 2) * \
+                       np.sqrt((box[1][1] - box[2][1]) ** 2 +
+                               (box[1][0] - box[2][0]) ** 2)
+
+                if size < 0.05 * image_size:
+                    continue
+
                 startX, startY = min(box, key=lambda x: x[0])[0], \
                                  min(box, key=lambda x: x[1])[1]
 
@@ -130,10 +136,10 @@ def detector(model, image_path, to_image_url):
                 outputs = \
                     torch.nn.Softmax()(model(croped_image)).data.cpu().numpy()[
                         0]
-                print(outputs)
+
                 ind = outputs.argsort()[-3:][::-1]
                 ind_max = outputs.argsort()[-1:][::-1]
-                if names[ind[0]] != 'garbage':
+                if names[ind[0]] != 'garbage1':
                     cv2.drawContours(image1, [box], 0, (0, 0, 255), 2)
                     label = ""
                     for i in ind:
@@ -147,10 +153,11 @@ def detector(model, image_path, to_image_url):
                     y, dy = startY, 15
                     for line in label.split('\n'):
                         y += dy
-                        cv2.putText(image1, line, (startX, y),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.4, 1)
+                        # cv2.putText(image1, line, (startX, y),
+                        #             cv2.FONT_HERSHEY_SIMPLEX, 0.4, 1)
 
-                    img[np.where((img == [0, 0, 0]).all(axis=2))] = [255, 255, 255]
+                    img[np.where((img == [0, 0, 0]).all(axis=2))] = [255, 255,
+                                                                     255]
                     images.append(img)
 
         return cv2.cvtColor(image1, cv2.COLOR_BGR2RGB), ingr
